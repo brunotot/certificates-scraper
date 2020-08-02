3d05f1751ee25c044577e2125a4c7001fced6b88
Bruno <53398175+brunotot@users.noreply.github.com>
2020-08-02 15:40:43 +0200

    IecWebsite.java indentation fix

--- src/iec/IecWebsite.java
+++ src/iec/IecWebsite.java
@@ -12,7 +12,7 @@ import com.codeborne.selenide.WebDriverRunner;
 
 public class IecWebsite {
 	
-	private static IecWebsite instance;
+    private static IecWebsite instance;
 
     private IecWebsite() {
 
